442d6b56b8e6645e8ae51bc4a6b5e4d11069f475
Patrick Spiegel <35311793+Weeb6279@users.noreply.github.com>
2025-05-18 10:49:13 +0200

    Added generic streamerbot connection

---
+++ src/composables/useBotConnection.ts
@@ -0,0 +1,48 @@
+import { computed, toValue, watch } from 'vue'
+import type {StreamerbotInfo} from '@streamerbot/client'
+import { useStreamerbot, type UseStreamerbotOptions } from '@streamerbot/vue'
+
+export function useBotConnection<T>(
+  channel: string,
+  callback: (data: T) => Promise<void>,
+  options: UseStreamerbotOptions = {},
+) {
+  const _options = computed(() => (typeof options === 'function' ? options() : toValue(options)))
+
+  console.log(_options)
+
+  const { data, connect, disconnect } = useStreamerbot({
+    host: _options.value?.host,
+    port: _options.value?.port,
+    password: _options.value?.password,
+    immediate: _options.value?.immediate,
+    autoReconnect: _options.value?.autoReconnect,
+    retries: _options.value?.retries,
+    onConnect: onConnectToStreamerBot,
+    onError: _options.value?.onError,
+    subscribe: {
+      General: ['Custom'],
+    },
+  })
+
+  function onConnectToStreamerBot(data: StreamerbotInfo) {
+    if (_options.value?.onConnect != null) {
+      _options.value.onConnect(data);
+    }
+
+    console.log("Connected to streamer bot %o", data)
+  }
+
+  watch(data, (val) => {
+    if (val.data == null) return
+
+    if (val.data.channel === channel) {
+      callback(val.data.content as T).then(() => {})
+    }
+  })
+
+  return {
+    connect,
+    disconnect
+  }
+}
